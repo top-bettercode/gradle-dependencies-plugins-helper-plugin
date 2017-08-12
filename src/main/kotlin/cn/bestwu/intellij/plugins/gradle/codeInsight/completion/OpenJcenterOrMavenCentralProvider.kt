0aac512e485a60f47f6e0c9ea2908ec03887a0a7
Peter Wu <piterwu@outlook.com>
2017-08-12 16:37:44 +0800

    增加 OpenJcenterOrMavenCentralProvider

---
+++ src/main/kotlin/cn/bestwu/intellij/plugins/gradle/codeInsight/completion/OpenJcenterOrMavenCentralProvider.kt
@@ -0,0 +1,54 @@
+package cn.bestwu.intellij.plugins.gradle.codeInsight.completion
+
+import com.intellij.lang.documentation.DocumentationProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiManager
+import org.jetbrains.kotlin.psi.KtStringTemplateExpression
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrLiteral
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrStringContent
+
+class OpenJcenterOrMavenCentralProvider : DocumentationProvider {
+
+    companion object {
+        private const val GROUP_AND_ARTIFACT: String = "http://search.maven.org/#search|gav|1|g:\"%s\" AND a:\"%s\""
+        private const val GROUP: String = "http://search.maven.org/#search|gav|1|g:\"%s\""
+    }
+
+    override fun getQuickNavigateInfo(element: PsiElement?, element1: PsiElement?): String? {
+        return null
+    }
+
+    override fun getUrlFor(element: PsiElement?, element1: PsiElement?): List<String>? {
+        return null
+    }
+
+    override fun generateDoc(element: PsiElement, element1: PsiElement?): String? {
+        if (element is GrLiteral || element is GrStringContent || element is KtStringTemplateExpression) {
+            var e = element
+            do {
+                e = e.parent ?: return null
+            } while ("dependencies" != e.firstChild.text && "imports" != e.firstChild.text)
+            if (element1 != null) {
+                val dependency = element.text.trim('"','\'')
+                val mavenUrl = split(dependency).let {
+                    if (it.size >= 2) {
+                        GROUP_AND_ARTIFACT.format(it[0], it[1])
+                    } else {
+                        GROUP.format(it[0])
+                    }
+                }
+                return "<a href=\"https://bintray.com/search?query=$dependency\">search in jcenter</a><br/>" +
+                        "<a href='$mavenUrl'>search in mavenCentral</a><br/>"
+            }
+        }
+        return null
+    }
+
+    override fun getDocumentationElementForLookupItem(manager: PsiManager?, o: Any?, element: PsiElement?): PsiElement? {
+        return null
+    }
+
+    override fun getDocumentationElementForLink(manager: PsiManager?, s: String?, element: PsiElement?): PsiElement? {
+        return null
+    }
+}
