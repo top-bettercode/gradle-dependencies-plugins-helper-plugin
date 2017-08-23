bd1ab3827948cf536991530d5f743babc128ef02
Peter Wu <piterwu@outlook.com>
2017-08-23 08:53:15 +0800

    plugin.xml调整;版本号变为0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("org.jetbrains.intellij") version "0.2.16"
 }
 group = "cn.bestwu"
-version = "0.0.2"
+version = "0.0.3"
 intellij {
     updateSinceUntilBuild = false
 //    version = "IC-2017.2.2"
