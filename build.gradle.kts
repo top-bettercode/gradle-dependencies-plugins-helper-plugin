df0108ba6e395ba2e947daa8e2b07815604a100f
Peter Wu <piterwu@outlook.com>
2020-06-30 08:55:20 +0800

    发布设置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,3 @@
-/*
- * Copyright (c) 2017 Peter Wu
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Copyright (c) 2017 Peter Wu
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import org.jetbrains.intellij.tasks.PublishTask
 
 plugins {
@@ -45,7 +13,7 @@ intellij {
     updateSinceUntilBuild = false
 //    downloadSources = false
     version = ideaVersion as String
-    setPlugins("java","Groovy", "gradle", "Kotlin", "properties", "junit")
+    setPlugins("java", "Groovy", "gradle", "Kotlin", "junit")
 }
 
 tasks.withType(PublishTask::class.java) {
@@ -60,6 +28,5 @@ repositories {
 
 dependencies {
     implementation("org.jsoup:jsoup:1.13.1")
-
 }
 
