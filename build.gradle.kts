4190be8379ac2d815ae17acfd08f6281390b184a
Peter Wu <piterwu@outlook.com>
2022-12-01 14:17:17 +0800

    idea配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("java")
     id("org.jetbrains.intellij") version "1.10.0"
-    id("org.jetbrains.kotlin.jvm") version "1.7.21"
+    kotlin("jvm") version "1.7.22"
 }
 
 group = "cn.bestwu"
 version = "0.1.8"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 intellij {
     version.set("2022.2.4")
     plugins.set(listOf("java", "Groovy", "gradle", "Kotlin", "junit"))
+    intellij.updateSinceUntilBuild.set(false)
 }
 
 tasks {
@@ -27,11 +30,11 @@ tasks {
     }
 
     compileKotlin {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 
     publishPlugin {
@@ -47,5 +50,14 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
 }
 
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
